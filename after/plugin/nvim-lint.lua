8ec803e7cc0c2df9f8de75e959b6226ee664a51d
Pranav Sindura <pranav.sindura@gmail.com>
2025-01-02 20:07:15 +0530

    gruvbox, xml, oil dir search

--- after/plugin/nvim-lint.lua
+++ after/plugin/nvim-lint.lua
@@ -1,21 +1,21 @@
 local lint = require("lint")
 lint.linters_by_ft = {
-	markdown = { "markdownlint" },
-	javascript = { "eslint" },
-	typescript = { "eslint" },
-	javascriptreact = { "eslint" },
-	typescriptreact = { "eslint" },
-	json = { "jsonlint" },
-	html = { "htmlhint" },
-	python = { "pylint" },
+  markdown = { "markdownlint" },
+  javascript = { "eslint" },
+  typescript = { "eslint" },
+  javascriptreact = { "eslint" },
+  typescriptreact = { "eslint" },
+  json = { "eslint" },
+  html = { "htmlhint" },
+  python = { "pylint" },
 }
 
 -- Create autocommand which carries out the actual linting
 -- on the specified events.
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-	group = lint_augroup,
-	callback = function()
-		require("lint").try_lint()
-	end,
+  group = lint_augroup,
+  callback = function()
+    require("lint").try_lint()
+  end,
 })
--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,5 +1,20 @@
-require("oil").setup({
-	view_options = {
-		show_hidden = true,
-	},
+local oil = require('oil')
+oil.setup({
+  view_options = {
+    show_hidden = true,
+  },
+  lsp_file_methods = {
+    enabled = true,
+    timeout_ms = 5000,
+    autosave_changes = "unmodified"
+  },
+  keymaps = {
+    ["<C-f>"] = function()
+      local cwd = oil.get_current_dir()
+      require('telescope.builtin').grep_string({
+        search = vim.fn.input("Grep [" .. cwd .. "] > "),
+        cwd = cwd
+      })
+    end
+  }
 })
