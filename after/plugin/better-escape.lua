61f865f1cd4c0842144b0f02a4fd9eacd3e3e9ff
Pranav Sindura <pranav.sindura@gmail.com>
2025-02-25 08:43:31 +0530

    remove jj from better escape

--- after/plugin/better-escape.lua
+++ after/plugin/better-escape.lua
@@ -1,1 +1,32 @@
-require("better_escape").setup()
+require("better_escape").setup {
+  timeout = vim.o.timeoutlen,
+  default_mappings = true,
+  mappings = {
+    i = {
+      j = {
+        -- These can all also be functions
+        k = "<Esc>",
+      },
+    },
+    c = {
+      j = {
+        k = "<Esc>",
+      },
+    },
+    t = {
+      j = {
+        k = "<C-\\><C-n>",
+      },
+    },
+    v = {
+      j = {
+        k = "<Esc>",
+      },
+    },
+    s = {
+      j = {
+        k = "<Esc>",
+      },
+    },
+  },
+}
